bcfe892e8821a244f1ee8aff88aac7a8dcb19848
Julio Silva <julio.silva@criticalsoftware.com>
2022-10-24 10:06:48 +0100

    removendo o projeto em java

--- src/main/java/com/julioafonsso/investimentos/model/acao/AcaoExcluidaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.julioafonsso.investimentos.model.acao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AcaoExcluidaRepository extends JpaRepository<AcaoExcluida, String> {
-}
--- src/main/java/com/julioafonsso/investimentos/model/fi/FundoImobiliarioExcluidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.julioafonsso.investimentos.model.fi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FundoImobiliarioExcluidoRepository
-        extends JpaRepository<FundoImobiliarioExcluido, String> {
-}
