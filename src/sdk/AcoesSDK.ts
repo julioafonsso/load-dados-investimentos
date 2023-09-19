68ac7215b0b5b5d10526b790ccf1d3398a041c79
Julio Afonso Martins da Silva <julio.afonso-martins-da-silva@mdoc.one>
2023-09-19 22:51:17 +0100

    remove usseles code

--- src/sdk/AcoesSDK.ts
+++ src/sdk/AcoesSDK.ts
@@ -1,37 +1,24 @@
 import axios from "axios";
-import { Acao } from "../entities/Acao";
+import {Acao} from "../entities/Acao";
 
 const CATEGORY = {
-    BRASIL : 1,
-    USA : 12
+    BRASIL: 1,
+    USA: 12
 }
 
 const SETOR = {
-    BEN_INDUSTRIAIS : 1,
-    CONSUMO_CICLICO : 2,
-    CONSUMO_NAO_CICLICO : 3,
-    FINANCEIROS_OUTROS : 4,
-    MATERIAIS_BASICO : 5,
-    PETROLEO_GAS : 6,
-    SAUDE : 7,
-    TECNLOGIA_INFORMACAO : 8,
-    COMUNICACAO : 9,
-    UTILIDADE_PUBLICA : 10
+    BEN_INDUSTRIAIS: 1,
+    CONSUMO_CICLICO: 2,
+    CONSUMO_NAO_CICLICO: 3,
+    FINANCEIROS_OUTROS: 4,
+    MATERIAIS_BASICO: 5,
+    PETROLEO_GAS: 6,
+    SAUDE: 7,
+    TECNLOGIA_INFORMACAO: 8,
+    COMUNICACAO: 9,
+    UTILIDADE_PUBLICA: 10
 }
 
-const CATEGORY_ACAO_BRASIL = 1;
-const CATEGORY_ACAO_USA = 12;
-
-const SETOR_BEN_INDUSTRIAIS = 1;
-const SETOR_CONSUMO_CICLICO = 2;
-const SETOR_CONSUMO_NAO_CICLICO = 3;
-const SETOR_FINANCEIROS_OUTROS = 4;
-const SETOR_MATERIAIS_BASICO = 5;
-const SETOR_PETROLEO_GAS = 6;
-const SETOR_SAUDE = 7;
-const SETOR_TECNLOGIA_INFORMACAO = 8;
-const SETOR_COMUNICACAO = 9;
-const SETOR_UTILIDADE_PUBLICA = 10;
 
 export type AcaoType = {
     categoryId: number,
@@ -44,34 +31,34 @@ export type AcaoType = {
     ticker: string;
 };
 
-export const getAcoes = async() : Promise<Acao[]> =>{
-      let res:  Acao[] = [];
+export const getAcoes = async (): Promise<Acao[]> => {
+    let res: Acao[] = [];
 
-     let categorias =  Object.values(CATEGORY)
-    let setores =  Object.values(SETOR)
+    let categorias = Object.values(CATEGORY)
+    let setores = Object.values(SETOR)
 
 
-      for(const categoria  of categorias) {
-          for (const setor of setores) {
-              res.push(...await call(categoria, setor))
-          }
-      }
+    for (const categoria of categorias) {
+        for (const setor of setores) {
+            res.push(...await call(categoria, setor))
+        }
+    }
 
-      return res;
+    return res;
 };
 
-const call = async (categoryType: number, setorId: number) :  Promise<Acao[]> =>{
+const call = async (categoryType: number, setorId: number): Promise<Acao[]> => {
 
-  const url = `https://statusinvest.com.br/sector/getcompanies?categoryType=${categoryType}&SetorId=${setorId}`;
-  
-  const res = await axios.get(
-      url
-      ,
-      {
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    const url = `https://statusinvest.com.br/sector/getcompanies?categoryType=${categoryType}&SetorId=${setorId}`;
+
+    const res = await axios.get(
+        url
+        ,
+        {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+            }
         }
-      }
     );
     return res.data.data;
 };
